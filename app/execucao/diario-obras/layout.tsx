3787bbc4ea9d35c960672c44fef422f6e1edfb7b
fabionicodemus-lang <fabio.nicodemus@gmail.com>
2026-07-28 17:35:44 -0300

    Execução: carregar correção do fluxo de criação do diário

---
+++ app/execucao/diario-obras/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./daily-log-create-fix.css";
+
+export default function DailyLogLayout({ children }: { children: ReactNode }) {
+  return children;
+}
