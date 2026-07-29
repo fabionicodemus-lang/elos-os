a68c02be786484cbaf26a7fbaf07f924780fc87f
fabionicodemus-lang <fabio.nicodemus@gmail.com>
2026-07-29 12:11:06 -0300

    Financeiro: aplicar estilos compactos ao importador XML

---
+++ app/financeiro/notas-eletronicas/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "../../finance-electronic-invoice-import-fix.css";
+
+export default function ElectronicInvoicesLayout({ children }: { children: ReactNode }) {
+  return children;
+}
