c015ce38514b969b265578357725e3dcf288cd3e
fabionicodemus-lang <fabio.nicodemus@gmail.com>
2026-07-29 13:52:32 -0300

    Financeiro: carregar estilos das contas bancárias

---
+++ app/financeiro/contas-bancarias/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "../../finance-bank-accounts.css";
+
+export default function BankAccountsLayout({ children }: { children: ReactNode }) {
+  return children;
+}
